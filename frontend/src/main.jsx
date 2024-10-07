61850bde61a81ed889a37778ab53b32c1ea9a15a
yashica1511 <mailtoyashi04@gmail.com>
2024-10-08 01:02:46 +0530

    Backend related pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './context/AuthContext';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter as Router } from 'react-router-dom'; 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router> {/* Wrap the entire app with BrowserRouter here */}
-      <AuthProvider>
+    <Router> 
         <App />
-      </AuthProvider>
     </Router>
   </React.StrictMode>
 );
